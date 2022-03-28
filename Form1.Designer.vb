0be4a43d53ef281a1f6e99da7b2f148f123a03e6
Administrator <Administrator@PSPKM2826-2242>
2022-03-28 12:30:18 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.lblResult2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(143, 123)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(86, 33)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(156, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 37)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "BMI"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label2.Location = New System.Drawing.Point(52, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Weight (kg)      :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label3.Location = New System.Drawing.Point(52, 92)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(93, 17)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Height (m) ^2  :"
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(156, 53)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(100, 23)
+        Me.txt1.TabIndex = 4
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(156, 89)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(100, 23)
+        Me.txt2.TabIndex = 5
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(143, 172)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(86, 25)
+        Me.lblResult.TabIndex = 6
+        '
+        'lblResult2
+        '
+        Me.lblResult2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult2.Location = New System.Drawing.Point(143, 209)
+        Me.lblResult2.Name = "lblResult2"
+        Me.lblResult2.Size = New System.Drawing.Size(86, 25)
+        Me.lblResult2.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(417, 285)
+        Me.Controls.Add(Me.lblResult2)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents txt2 As TextBox
+    Friend WithEvents lblResult As Label
+    Friend WithEvents lblResult2 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Dim h As Double
+    Dim w As Double
+    Dim t As Double
+    Dim Result As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        w = txt1.Text
+        h = txt2.Text
+        t = w / (h * h)
+
+        If t <= 18.5 Then
+            Result = "Underweight"
+
+        ElseIf t >= 18.5 And t <= 24.9 Then
+            Result = "Normal weight"
+        ElseIf t >= 25 And t <= 29.9 Then
+            Result = "Overwight"
+        ElseIf t > 30 Then
+            Result = "Obesity"
+        Else
+            Result = "Please Insert Number only"
+        End If
+        lblResult.Text = Format(t, "0.00")
+        lblResult2.Text = Result
+
+    End Sub
+End Class
